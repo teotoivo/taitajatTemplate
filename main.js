c642f77f4af3eb16435e3ca8d77eae938b22f643
teotoivo <teo.maximilien@gmail.com>
2023-01-30 12:28:09 +0200

    s

--- main.js
+++ main.js
@@ -3,5 +3,15 @@ $(function () {
     $(".menu").toggleClass("open");
     $("nav").toggleClass("open");
     $("header").toggleClass("open");
+    $(document).on("click", function (event) {
+      if (!$(event.target).closest("nav").length) {
+        // Click happened outside of the element
+        $(".menu").toggleClass("open");
+        $("nav").toggleClass("open");
+        $("header").toggleClass("open");
+        //remove event listerer
+        $(document).off("click");
+      }
+    });
   });
 });
